d3a1baec9ec38aae8ba051cd54eac07b8bf30d66
Phil McCarthy <philmcc2631@gmail.com>
2021-02-12 10:20:33 -0700

    Fix capybara selector and match search nation title to view

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'search index', type: :feature do
   describe 'as a user' do
     it 'i see the total number of people who live in the nation i selected' do
-      search_nation = 'Fire Nation'
+      search_nation = 'fire nation'
       
-       visit root_path
+      visit root_path
 
-      find("option[value='fire_nation']").click
+      select('Fire Nation', from: :nation)
       click_button 'Search For Members'
 
-      expect(page).to have_content("Members of #{search_nation}: 111")
+      expect(page).to have_content("Members of #{search_nation.titleize}: 97")
 
     end
   end

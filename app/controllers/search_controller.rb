bd074fc8e14d054247bdead942cbbd7bcd63c212
Phil McCarthy <philmcc2631@gmail.com>
2021-02-12 10:15:08 -0700

    Update search params to match form, add search view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,8 @@
 class SearchController < ApplicationController
   def index
-    @search_nation = params[:search]
-    response = Faraday.get('https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=497&affiliation=fire')
+    @search_nation = params[:nation].gsub(/_/, ' ')
+    response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=497&affiliation=#{@search_nation}")
     data = JSON.parse(response.body, symbolize_names: true)
-    
     @characters = data.map do |character_data|
       Character.new(character_data)
     end

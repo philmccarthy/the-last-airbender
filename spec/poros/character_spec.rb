d0c1651d4cbe78dc4b0efaedb6e0ba193ec52079
Phil McCarthy <philmcc2631@gmail.com>
2021-02-12 10:34:49 -0700

    Add poro tests for enemies & allies methods to return none if empty

--- spec/poros/character_spec.rb
+++ spec/poros/character_spec.rb
@@ -2,23 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Character do
   describe 'it exists' do
-    it 'assigns json data to attributes' do
+    before(:each) do
       attributes = {
-        _id: "5cf5679a915ecad153ab68cc",
         allies: ["Fire Nation"],
         enemies: ["Aang"],
         photoUrl: "https://vignette.wikia.nocookie.net/avatar/images/2/24/Afiko.png/revision/latest?cb=20121121024128",
         name: "Afiko",
         affiliation: "Fire Nation"
       }
+      @afiko = Character.new(attributes)
+    end
 
-      afiko = Character.new(attributes)
+    it 'assigns json data to attributes' do
+      expect(@afiko.name).to be_a String
+      expect(@afiko.allies).to be_a String
+      expect(@afiko.enemies).to be_a String
+      expect(@afiko.affiliation).to be_a String
+      expect(@afiko.photo).to be_a String
+    end
 
-      expect(afiko.name).to be_a String
-      expect(afiko.allies).to be_an Array
-      expect(afiko.enemies).to be_an Array
-      expect(afiko.affiliation).to be_a String
-      expect(afiko.photo).to be_a String
+    it 'returns allies and enemies as a string, or else none' do
+      expect(@afiko.allies).to eq('Fire Nation')
+      expect(@afiko.enemies).to eq('Aang')
+
+      attributes =  {
+        allies: [],
+        enemies: [],
+        photoUrl: "https://vignette.wikia.nocookie.net/avatar/images/1/16/Ember_Island_Players.png/revision/latest?cb=20130722113209",
+        name: "Ember Island Players",
+        affiliation: "Fire Nation"
+      }
+      @no_enemies_or_allies = Character.new(attributes)
+
+      expect(@no_enemies_or_allies.allies).to eq('None')
+      expect(@no_enemies_or_allies.enemies).to eq('None')
     end
   end
 end
